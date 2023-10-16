90181c97f1e01b3bf2a8b5dc55421fe30c1bed98
Skeith <github@skeith.dev>
2023-10-17 07:09:43 +0800

    Add the app header implementation and add it to the global layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Web3ContextProvider from '../contexts/Web3Context';
 import ThemeContextProvider from '../contexts/ThemeContext';
+import AppHeader from '../components/AppHeader';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={inter.className}>
           <ThemeContextProvider>
             <Web3ContextProvider>
+                <AppHeader/>
                 {children}
             </Web3ContextProvider>
           </ThemeContextProvider>
